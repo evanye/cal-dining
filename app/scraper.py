9038d48ba4a3161edd838df84f6a50cb02746aca
Evan Ye <applepie31415@gmail.com>
2013-05-26 03:06:10 -0700

    Updated scraper for several edge cases

--- app/scraper.py
+++ app/scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime
+from models import Food, Menu
 
 BASE_URL = 'http://services.housing.berkeley.edu/FoodPro/dining/static/'
 MENU_URL = 'todaysentrees.asp'
@@ -29,12 +31,13 @@ def crawl():
 			meal_dc_data = meal_data.contents[dc_index]
 
 			for entry in meal_dc_data.find_all('a'):
+				meal_name = entry.contents[0].string
+
 				# skip the "Nutritive Analysis" link
-				if 'nutritive analysis' in entry.contents[0].string.lower():
+				if 'nutritive analysis' in meal_name.lower():
 					continue
 
-				meal_name = extract_food_info(entry)
-				
+				food_data = extract_food_info(entry)			
 
 def extract_food_info(entry):
 	link = BASE_URL + entry['href']
@@ -58,16 +61,21 @@ def extract_food_info(entry):
 	# crawl link (declared above) to check allergens
 	allergens, ingredients = get_allergens_and_ingredients(link)
 
-	print meal_name, allergens, ingredients, vegan, vegetarian, gluten_free
-	return entry;
+	return Food(name = meal_name, allergens = allergens, ingredients = ingredients, \
+	vegan = vegan, vegetarian = vegetarian, gluten_free = gluten_free)
 
 def get_allergens_and_ingredients(link):
 	html = http_get(link)
 	html = html.find_all(face="arial", size="2")[-2:]
 
-	allergens = html[0].contents[1].string if len(html[0].contents) > 1 else None
-	ingredients = html[1].contents[1].string if len(html[1].contents) > 1 else None
+	# edge case where no nutrition info is avaiable
+	if len(html) <= 1:
+		return "n/a", "n/a"
+
+	allergens = html[0].contents[1].string if len(html[0].contents) > 1 else ""
+	ingredients = html[1].contents[1].string if len(html[1].contents) > 1 else ""
 
 	return allergens, ingredients
 
-a = crawl()
+def get_date():
+	return datetime.now().replace(hour = 0,  minute = 0, second = 0, microsecond = 0)
