f57b048c0d109479164a640e8f02a001a44d1062
Evan Ye <applepie31415@gmail.com>
2013-05-26 16:05:41 -0700

    Fixed scraper - database integration

--- app/models.py
+++ app/models.py
@@ -5,14 +5,18 @@ class Food(db.Model):
 	name = db.Column(db.String(64), index = True, unique = True)
 	allergens = db.Column(db.String(512))
 	ingredients = db.Column(db.String(2048))
-	vegan = db.Column(db.Boolean(), index = True)
-	vegetarian = db.Column(db.Boolean(), index = True)
-	gluten_free = db.Column(db.Boolean())
+	vegan = db.Column(db.Boolean, index = True)
+	vegetarian = db.Column(db.Boolean, index = True)
+	gluten_free = db.Column(db.Boolean)
+	meals = db.relationship('Menu', backref = 'food', lazy='dynamic')
 
 	def __repr__(self):
 		return "Food: {0}, vegan: {1}, vegetarian: {2}, GF: {3}, \
 		allergens: {4}, ingredients: {5}".format(self.name, \
 		self.vegan, self.vegetarian, self.gluten_free, self.allergens, self.ingredients)
+
+LOCATION_TO_ENUM = {'crossroads': 1, 'cafe3': 2, 'foothill': 3, 'clarkkerr': 4}
+ENUM_TO_LOCATION = {1: 'crossroads', 2: 'cafe3', 3: 'foothill', 4: 'clarkkerr'}
 
 MEAL_TO_ENUM = {'breakfast': 1, 'lunch': 2, 'dinner': 3}
 ENUM_TO_MEAL = {1: 'breakfast', 2: 'lunch', 3: 'dinner'}
@@ -20,10 +24,11 @@ ENUM_TO_MEAL = {1: 'breakfast', 2: 'lunch', 3: 'dinner'}
 class Menu(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	date = db.Column(db.Date, index = True)
-	location = db.Column(db.String(64), index = True)
+	location = db.Column(db.Integer, index = True)
 	meal = db.Column(db.Integer, index = True)
-	food = db.Column(db.Integer, db.ForeignKey('food.id'))
+	food_id = db.Column(db.Integer, db.ForeignKey('food.id'))
 
 	def __repr__(self):
-		return "Date: {0}, Located: {1}, Meal: {2}, Food: {3}".format(date, location, meal, food)
+		return "Date: {0}, Located: {1}, Meal: {2}, <{3}>"\
+		.format(self.date, self.location, self.meal, self.food)
 	
--- app/scraper.py
+++ app/scraper.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
-from models import Food, Menu
+from app import db
+from models import Food, Menu, LOCATION_TO_ENUM, MEAL_TO_ENUM
 
 BASE_URL = 'http://services.housing.berkeley.edu/FoodPro/dining/static/'
 MENU_URL = 'todaysentrees.asp'
@@ -9,17 +10,14 @@ MEALS = {'breakfast':3, 'lunch': 5, 'dinner': 7}
 DINING_COMMONS = {'crossroads':1, 'cafe3': 3, 'foothill': 5, 'clarkkerr': 7}
 VEGE_LEGEND = {'vegan':'#800040', 'vegetarian':'#008000'}
 
-def http_get(url):
-	resp = requests.get(url)
-	return BeautifulSoup(resp.text, "lxml")
-
 def crawl():
 	# make web request
 	soup = http_get(BASE_URL + MENU_URL)
-
 	# locate html data
 	html = soup.body.contents[-1].table.tbody.contents[3].td.table.contents
-	data = {}
+
+	# stores food that has already been added to the table
+	food_cache = {}
 
 	# extract data
 	for MEAL in MEALS:
@@ -32,24 +30,36 @@ def crawl():
 
 			for entry in meal_dc_data.find_all('a'):
 				meal_name = entry.contents[0].string
+				meal_name, gluten_free = truncate_meal_name(meal_name)
 
 				# skip the "Nutritive Analysis" link
 				if 'nutritive analysis' in meal_name.lower():
 					continue
 
-				food_data = extract_food_info(entry)			
+				# create database models object
+				if meal_name in food_cache:	 
+					food_obj = food_cache[meal_name]
+				else: # food is not located in local cache
+					  # check if food is in database
+					food_obj = Food.query.filter_by(name=meal_name).first()
+					  # not found in database, crawl page
+					if food_obj is None:
+						food_obj = extract_food_info(entry)
+						db.session.add(food_obj)
+
+					  # add food to the cache
+					food_cache[meal_name] = food_obj
+
+				menu_obj = Menu(date = get_date(), location = LOCATION_TO_ENUM[DINING_COMMON], \
+				meal = MEAL_TO_ENUM[MEAL], food = food_obj)
+
+				db.session.add(menu_obj)
+	db.session.commit()
 
 def extract_food_info(entry):
 	link = BASE_URL + entry['href']
 	meal_name = entry.contents[0].string
-	gluten_free = False
-
-	# truncate Honey Bear or Gluten Free prefix
-	if meal_name[0:3] == 'HB ':
-		meal_name = meal_name[3:]
-	elif meal_name[0:3] == 'GF ':
-		meal_name = meal_name[3:]
-		gluten_free = True
+	meal_name, gluten_free = truncate_meal_name(meal_name)
 
 	# check for vegan and vegetarian foods
 	vegan, vegetarian = False, False
@@ -76,6 +86,21 @@ def get_allergens_and_ingredients(link):
 	ingredients = html[1].contents[1].string if len(html[1].contents) > 1 else ""
 
 	return allergens, ingredients
+
+# truncate Honey Bear or Gluten Free prefix
+# 	returns pair of meal name, and if its gluten free or not
+def truncate_meal_name(meal_name):
+	gluten_free = False
+	if meal_name[0:3] == 'HB ':
+		meal_name = meal_name[3:]
+	elif meal_name[0:3] == 'GF ':
+		meal_name = meal_name[3:]
+		gluten_free = True
+	return meal_name, gluten_free
+
+def http_get(url):
+	resp = requests.get(url)
+	return BeautifulSoup(resp.text, "lxml")
 
 def get_date():
 	return datetime.now().replace(hour = 0,  minute = 0, second = 0, microsecond = 0)
